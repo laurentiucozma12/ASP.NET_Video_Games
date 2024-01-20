8a9f00fea82774a35ab9a8972940d2fc68324de6
Lau <laurentiucozma12@gmail.com>
2024-01-20 02:25:04 +0200

    genres and platforms done

--- MyVideoGameProject/Controllers/GenresController.cs
+++ MyVideoGameProject/Controllers/GenresController.cs
@@ -1,14 +1,169 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VideoGameModel.Data;
+using VideoGameModel.Models;
 
-namespace MyVideoGameProject.Controllers
+namespace MyVideoGameProject
 {
     [Authorize(Roles = "Employee")]
     public class GenresController : Controller
     {
-        public IActionResult Index()
+        private readonly VideoGameContext _context;
+
+        public GenresController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Genres
+        public async Task<IActionResult> Index()
+        {
+            return _context.Genres != null ?
+                View(await _context.Genres.ToListAsync()) :
+                Problem("Entity set 'VideoGameContext.Genres'  is null.");
+        }
+
+        // GET: Genres/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // GET: Genres/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Genres/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(genre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(genre);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GenreExists(genre.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // POST: Genres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Genres == null)
+            {
+                return Problem("Entity set 'VideoGameContext.Genres' is null.");
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre != null)
+            {
+                _context.Genres.Remove(genre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        private bool GenreExists(int id)
+        {
+            return (_context.Genres?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
--- MyVideoGameProject/Controllers/PlatformsController.cs
+++ MyVideoGameProject/Controllers/PlatformsController.cs
@@ -1,14 +1,169 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VideoGameModel.Data;
+using VideoGameModel.Models;
 
-namespace MyVideoGameProject.Controllers
+namespace MyVideoGameProject
 {
     [Authorize(Roles = "Employee")]
     public class PlatformsController : Controller
     {
-        public IActionResult Index()
+        private readonly VideoGameContext _context;
+
+        public PlatformsController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Platforms
+        public async Task<IActionResult> Index()
+        {
+            return _context.Platforms != null ?
+                View(await _context.Platforms.ToListAsync()) :
+                Problem("Entity set 'VideoGameContext.Platforms'  is null.");
+        }
+
+        // GET: Platforms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Platforms == null)
+            {
+                return NotFound();
+            }
+
+            var platform = await _context.Platforms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (platform == null)
+            {
+                return NotFound();
+            }
+
+            return View(platform);
+        }
+
+        // GET: Platforms/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Platforms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Platform platform)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(platform);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(platform);
+        }
+
+        // GET: Platforms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Platforms == null)
+            {
+                return NotFound();
+            }
+
+            var platform = await _context.Platforms.FindAsync(id);
+            if (platform == null)
+            {
+                return NotFound();
+            }
+            return View(platform);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Platform platform)
+        {
+            if (id != platform.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(platform);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlatformExists(platform.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(platform);
+        }
+
+        // GET: Platforms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Platforms == null)
+            {
+                return NotFound();
+            }
+
+            var platform = await _context.Platforms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (platform == null)
+            {
+                return NotFound();
+            }
+
+            return View(platform);
+        }
+
+        // POST: Platforms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Platforms == null)
+            {
+                return Problem("Entity set 'VideoGameContext.Platforms' is null.");
+            }
+
+            var platform = await _context.Platforms.FindAsync(id);
+            if (platform != null)
+            {
+                _context.Platforms.Remove(platform);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        private bool PlatformExists(int id)
+        {
+            return (_context.Platforms?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
